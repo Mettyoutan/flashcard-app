ace3c63e13296f72f2f4f2acf5d7f703442d6df0
Mettyoutan <Metiu.tan@gmail.com>
2026-07-09 10:11:43 +0700

    feat: home page reflects login state, adds logout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,69 +1,54 @@
 "use client";
 
-import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { apiFetch } from "@/lib/api";
 
-export default function Home() {
-  const [] = useState();
+/*
+  Kenapa pakai useEffect?
+  - Kalau langsung ubah sesuatu tanpa useEffect, di component langsung, maka 
+    tiap kali render component, effect tersebut jalan terus menerus
+*/
+
+export default function HomePage() {
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Use effect dijalankan setelah component selesai render
+  // Digunakan untuk akses dunia luar
+  useEffect(() => {
+    setIsLoggedIn(Boolean(localStorage.getItem("access_token")));
+    setIsLoading(false);
+  }, []);
+
+  async function handleLogout() {
+    await apiFetch("/auth/logout", {
+      method: "POST",
+    });
+    // Remove access from localStorage
+    localStorage.removeItem("access_token");
+    setIsLoggedIn(false);
+  }
+
+  if (isLoading) return null;
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-2xl font-bold">Flashcard App</h1>
+
+      {/* Logika cek apakah sudah login */}
+      {isLoggedIn ? (
+        <Button onClick={handleLogout}>Logout</Button>
+      ) : (
+        <div className="flex gap-4">
+          <Button onClick={() => router.push("/login")}>Login</Button>
+          <Button variant="outline" onClick={() => router.push("/signin")}>
+            Daftar
+          </Button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      )}
     </div>
   );
 }
